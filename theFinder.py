bf00f7d4b651dd95591bf6f06fd3c63ddf41d2a3
YeemBoi <47729640+YeemBoi@users.noreply.github.com>
2019-05-11 13:04:19 -0700

    Add files via upload

---
+++ theFinder.py
@@ -0,0 +1,29 @@
+"""
+Hello!
+I was very determined to use only one file, so the readme is inside of the code now.
+
+This program is basically a super simple Python script for finding all the English words that can be spelled with certain letters.
+For example, you can type in "hello" and it will output words like hole, hell, he, oh, etc.
+It uses mielestronk.com's Corncob list of English words as it's word list for finding words.
+To exit the session, enter *quit.
+I have a much more user-friendly word finder with a ton of options at yeemboi.pythonanywhere.com if you're interested.
+Hope you enjoy!
+"""
+import urllib.request
+print("Loading...")
+wordList = str(urllib.request.urlopen("http://www.mieliestronk.com/corncob_lowercase.txt").read())[2 : -5].split("\\r\\n")
+print("Ready.")
+inputText = input().lower()
+while not "*quit" in inputText:
+	baseLength = len(inputText)
+	words = []
+	for word in wordList:
+		if len(word) > baseLength: continue
+		success = True
+		for char in word:
+			if word.count(char) > inputText.count(char):
+				success = False
+				break
+		if success: words.append(word)
+	print("\n".join(words))
+	inputText = input().lower()
